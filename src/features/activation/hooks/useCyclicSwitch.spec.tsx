62e1836a2d424bc130da801341d95bad4bee62ad
Shogo Goto <gotoadmn0605@gmail.com>
2023-05-10 16:20:00 +0900

    cyclicにchildを返すhook

--- src/features/activation/hooks/useCyclicSwitch.spec.tsx
+++ src/features/activation/hooks/useCyclicSwitch.spec.tsx
@@ -1,8 +1,9 @@
-// import { renderHook } from "@testing-library/react"
-// import { render } from "@testing-library/react"
+import { renderHook } from "@testing-library/react"
+import { act } from "react-dom/test-utils"
 import { type ReactNode } from "react"
+import flattenChildren from "react-flatten-children"
 
-import { countChildren } from "./useCyclicSwitch"
+import useCyclicSwitch, { countChildren } from "./useCyclicSwitch"
 
 const e1 = <div></div>
 const e2 = (
@@ -34,4 +35,54 @@ describe.each<[ReactNode, number, string]>([
   })
 })
 
-// describe("", () => {
+describe("useCyclicSwitch", () => {
+  test("not element", () => {
+    const e = "string"
+    const { result } = renderHook(() => useCyclicSwitch(e))
+
+    expect(result.current.child).toBeNull()
+    act(() => {
+      result.current.cycle()
+    })
+    expect(result.current.child).toBeNull()
+  })
+
+  test("no children", () => {
+    const e = <div></div>
+    const { result } = renderHook(() => useCyclicSwitch(e))
+
+    expect(result.current.child).toBeNull()
+    act(() => {
+      result.current.cycle()
+    })
+    expect(result.current.child).toBeNull()
+  })
+
+  test("3 children", () => {
+    const e = (
+      <>
+        <>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </>
+      </>
+    )
+    const { result } = renderHook(() => useCyclicSwitch(e))
+
+    const children = flattenChildren(e)
+    expect(result.current.child).toEqual(children[0])
+    act(() => {
+      result.current.cycle()
+    })
+    expect(result.current.child).toEqual(children[1])
+    act(() => {
+      result.current.cycle()
+    })
+    expect(result.current.child).toEqual(children[2])
+    act(() => {
+      result.current.cycle()
+    })
+    expect(result.current.child).toEqual(children[0])
+  })
+})
--- src/features/activation/hooks/useCyclicSwitch.ts
+++ src/features/activation/hooks/useCyclicSwitch.ts
@@ -1,19 +1,42 @@
-import { Children, type ReactElement, type ReactNode } from "react"
+import { Children, type ReactNode, useState } from "react"
 
 import * as ReactIs from "react-is"
-
-import type { CyclicSwitchProps } from "../types"
+import flattenChildren from "react-flatten-children"
 
 export function countChildren(n: ReactNode): number {
-  if (ReactIs.typeOf(n) === undefined) {
+  if (ReactIs.isElement(n)) {
+    const c = n.props.children
+    return flattenChildren(c).length
+  } else {
     return Children.count(n)
   }
-  const c = (n as ReactElement).props.children
-  return ReactIs.isFragment(c)
-    ? Children.count(c.props.children)
-    : Children.count(c)
 }
 
-const useCyclicSwitch = (props: CyclicSwitchProps): void => {}
+type ReturnType = {
+  cycle: () => void
+  child: ReactNode
+}
+
+// 子要素の表示を切り替える
+const useCyclicSwitch = (n: ReactNode): ReturnType => {
+  const count = countChildren(n)
+  const [index, setIndex] = useState(0)
+
+  const cycle = (): void => {
+    if (count === 0) return
+    const nextIndex = (index + 1) % count
+    setIndex(nextIndex)
+  }
+
+  let child: ReactNode = null
+  if (ReactIs.isElement(n) && count > 0) {
+    child = flattenChildren(n.props.children)[index]
+  }
+
+  return {
+    cycle,
+    child,
+  }
+}
 
 export default useCyclicSwitch
