a0affde8d54a129f3ead593c69184f8682878242
Shogo Goto <gotoadmn0605@gmail.com>
2023-05-25 20:07:53 +0900

    useRotateChildrenの初期indexをnullableで設定できるように変更

--- src/features/activation/hooks/useRotateChildren.ts
+++ src/features/activation/hooks/useRotateChildren.ts
@@ -15,29 +15,32 @@ type ReturnFuncs = {
   increment: () => void
   decrement: () => void
   setChild: (n: ReactNode) => void
+  setIndex: (i: number) => void
 }
 
 export type ReturnType = [State, ReturnFuncs]
 
+type Index = number | null
+
 // 要素を切り替える
-const useRotateChildren = (n: Rotatable): ReturnType => {
+const useRotateChildren = (n: Rotatable, initIndex: Index = 0): ReturnType => {
   const children = rotatableToArray(n)
   const count = countChildren(children)
-  const [index, setIndexDefault] = useState(0)
+  const [index, setIndexDefault] = useState<Index>(initIndex)
 
   const increment = useCallback(() => {
     if (count === 0) return
-    const nextIndex = (index + 1) % count
+    const nextIndex = ((index ?? 0) + 1) % count
     setIndexDefault(nextIndex)
   }, [count, index])
 
   const decrement = useCallback(() => {
     if (count === 0) return
-    const prevIndex = (index - 1 + count) % count
+    const prevIndex = ((index ?? 0) - 1 + count) % count
     setIndexDefault(prevIndex)
   }, [count, index])
 
-  const _setIndex = useCallback(
+  const setIndex = useCallback(
     (i: number): void => {
       if (i < 0 || count <= i) {
         const msg = `${i} is out of index [0,${count})`
@@ -52,9 +55,9 @@ const useRotateChildren = (n: Rotatable): ReturnType => {
   const setChild = useCallback(
     (child: ReactNode) => {
       const i = children.findIndex((e) => isEqual(e, child))
-      _setIndex(i)
+      setIndex(i)
     },
-    [children, _setIndex]
+    [children, setIndex]
   )
 
   const child = children[index]
@@ -68,6 +71,7 @@ const useRotateChildren = (n: Rotatable): ReturnType => {
       increment,
       decrement,
       setChild,
+      setIndex,
     },
   ]
 }
