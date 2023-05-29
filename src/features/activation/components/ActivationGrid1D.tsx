b360d9d8ea3adc3209595c9903ec412177510f93
Shogo Goto <gotoadmn0605@gmail.com>
2023-05-30 00:06:12 +0900

    Ctrl+anyみたいなのが書きにくいのでuseHotkey導入

--- src/features/activation/components/ActivationGrid1D.tsx
+++ src/features/activation/components/ActivationGrid1D.tsx
@@ -1,20 +1,15 @@
 import { css } from "@emotion/react"
-import { useKey } from "react-use"
+import { useHotkeys } from "react-hotkeys-hook"
 
 import { useActivation } from "../hooks"
 
 import type { Activatables, Index } from "../types"
 import type { SerializedStyles } from "@emotion/react"
 import type { FC } from "react"
-import type { KeyFilter } from "react-use/lib/useKey"
 
 type StyleType = "horizon" | "vertical"
 
-type StyleProps = [
-  positiveKey: string,
-  negativeKey: string,
-  style: SerializedStyles
-]
+type StyleProps = [plusKey: string, minusKey: string, style: SerializedStyles]
 
 const horizonStyle = css`
   display: flex;
@@ -24,16 +19,8 @@ const nonStyle = css``
 
 const keyProps = (t: StyleType): StyleProps => {
   return t === "horizon"
-    ? ["ArrowRight", "ArrowLeft", horizonStyle]
-    : ["ArrowDown", "ArrowUp", nonStyle]
-}
-
-const withCtrlKeyFilter = (key: string): KeyFilter => {
-  return (ev) => ev.ctrlKey && ev.key === key
-}
-
-const withoutCtrlKeyFilter = (key: string): KeyFilter => {
-  return (ev) => !ev.ctrlKey && ev.key === key
+    ? ["right", "left", horizonStyle]
+    : ["down", "up", nonStyle]
 }
 
 type Props = {
@@ -43,19 +30,19 @@ type Props = {
 }
 
 const ActivationGrid1D: FC<Props> = (props) => {
-  const [positiveKey, negativeKey, style] = keyProps(props.styleType)
+  const [plusKey, minusKey, style] = keyProps(props.styleType)
   const [st, fn] = useActivation({
     children: props.children,
     style,
     index: props.initialIndex ?? null,
   })
 
-  useKey(withoutCtrlKeyFilter(positiveKey), fn.increment, {}, [st])
-  useKey(withoutCtrlKeyFilter(negativeKey), fn.decrement, {}, [st])
-  useKey(withCtrlKeyFilter(positiveKey), fn.setLastIndex, {}, [st])
-  useKey(withCtrlKeyFilter(negativeKey), fn.setFirstIndex, {}, [st])
+  useHotkeys(`${plusKey}`, fn.increment, {}, [st])
+  useHotkeys(`${minusKey}`, fn.decrement, {}, [st])
+  useHotkeys(`ctrl+${plusKey}`, fn.setLastIndex, {}, [st])
+  useHotkeys(`ctrl+${minusKey}`, fn.setFirstIndex, {}, [st])
+  useHotkeys("enter", fn.fireClick, {}, [st])
 
-  useKey("Enter", fn.fireClick, {}, [st])
   return (
     <div
       tabIndex={0}
