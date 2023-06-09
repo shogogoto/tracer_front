9c4bb6968733479631108a1ef737c37d91f3afde
Shogo Goto <gotoadmn0605@gmail.com>
2023-06-09 19:22:24 +0900

    add ConceptItem Component

---
+++ src/features/concept/components/ConceptItem.stories.ts
@@ -0,0 +1,29 @@
+import ConceptItem from "./ConceptItem"
+import { one } from "../utils"
+
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta = {
+  title: "concepts/Item",
+  component: ConceptItem,
+  tags: ["autodocs"],
+} satisfies Meta<typeof ConceptItem>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const c = one("X".repeat(16), "xxxx ".repeat(30))
+
+export const Line: Story = {
+  args: {
+    isLineStyle: true,
+    concept: c,
+  },
+}
+
+export const Card: Story = {
+  args: {
+    isLineStyle: false,
+    concept: c,
+  },
+}
