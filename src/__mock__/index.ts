a22dfc6a4b61cc7d48b56a5e275ceb2ec8f9d287
Shogo Goto <gotoadmn0605@gmail.com>
2023-06-09 16:50:33 +0900

    mswをfeature配下に分離

--- src/__mock__/index.ts
+++ src/__mock__/index.ts
@@ -1,1 +1,8 @@
-export * from "./server"
+import { setupServer } from "msw/node"
+
+import handlers from "@/features/concept/api/__mock__"
+
+const merged = [...handlers]
+
+// export const worker = setupWorker(...merged)
+export const server = setupServer(...merged)
