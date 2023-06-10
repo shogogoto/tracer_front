42575e547350ed68078896007e6b579dff424b8e
Shogo Goto <gotoadmn0605@gmail.com>
2023-06-10 15:04:46 +0900

    add TreeCount Component

---
+++ src/features/concept/components/TreeCount.stories.ts
@@ -0,0 +1,22 @@
+import { TreeCount } from "./TreeCount"
+
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta = {
+  title: "concepts/TreeCount",
+  component: TreeCount,
+  tags: ["autodocs"],
+} satisfies Meta<typeof TreeCount>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Example: Story = {
+  args: {
+    neighborUpperCount: 1,
+    neighborLowerCount: 1,
+    allUpperCount: 1,
+    allLowerCount: 1,
+    enableTooltipAtSolo: true,
+  },
+}
---
+++ src/features/concept/components/TreeCount.tsx
@@ -0,0 +1,29 @@
+import { FaTree } from "react-icons/fa"
+import { Tooltip } from "react-tooltip"
+
+import type { FC } from "react"
+
+type TreeProps = {
+  neighborUpperCount: number
+  neighborLowerCount: number
+  allUpperCount: number
+  allLowerCount: number
+  enableTooltipAtSolo?: boolean
+}
+
+export const TreeCount: FC<TreeProps> = (props) => {
+  const enable = props.enableTooltipAtSolo ?? false
+  return (
+    <a
+      data-tooltip-id="tree-count-tooltip"
+      data-tooltip-content="依存元:隣接数/総数 依存先:隣接数/総数"
+    >
+      {props.neighborLowerCount}/{props.allLowerCount}
+      <FaTree />
+      {props.neighborUpperCount}/{props.allUpperCount}
+      {enable && TreeCountToolTip}
+    </a>
+  )
+}
+
+export const TreeCountToolTip = <Tooltip id="tree-count-tooltip" />
