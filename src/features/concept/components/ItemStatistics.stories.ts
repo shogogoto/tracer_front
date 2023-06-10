98a592f31a68befd212dacab78e609a3456823cc
Shogo Goto <gotoadmn0605@gmail.com>
2023-06-10 15:31:38 +0900

    add ItemStatistics Component

---
+++ src/features/concept/components/ItemStatistics.stories.ts
@@ -0,0 +1,24 @@
+import ItemStatistics from "./ItemStatistics"
+
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta = {
+  title: "concepts/ItemStatistics",
+  component: ItemStatistics,
+  tags: ["autodocs"],
+} satisfies Meta<typeof ItemStatistics>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Example: Story = {
+  args: {
+    all_lower_count: 1,
+    all_upper_count: 1,
+    lower_neighbor_count: 1,
+    upper_neighbor_count: 1,
+    max_distance_from_roots: 1,
+    max_distance_from_leaves: 1,
+    enableTooltipAtSolo: true,
+  },
+}
---
+++ src/features/concept/components/ItemStatistics.tsx
@@ -0,0 +1,43 @@
+import { css } from "@emotion/react"
+
+import { TipDistance } from "./TipDistance"
+import { TreeCount } from "./TreeCount"
+
+import type { DependencyStatistics } from "../types"
+import type { FC } from "react"
+
+const itemStatsParent = css`
+  display: flex;
+  flex-wrap: wrap;
+
+  > a {
+    padding: 1rem;
+  }
+`
+
+type Props = {
+  enableTooltipAtSolo?: boolean
+} & DependencyStatistics
+
+const ItemStatistics: FC<Props> = (props) => {
+  const enable = props.enableTooltipAtSolo ?? false
+
+  return (
+    <div css={itemStatsParent}>
+      <TreeCount
+        neighborUpperCount={props.lower_neighbor_count}
+        allUpperCount={props.all_upper_count}
+        neighborLowerCount={props.lower_neighbor_count}
+        allLowerCount={props.all_upper_count}
+        enableTooltipAtSolo={enable}
+      />
+      <TipDistance
+        fromLeaf={props.max_distance_from_leaves}
+        fromRoot={props.max_distance_from_roots}
+        enableTooltipAtSolo={enable}
+      />
+    </div>
+  )
+}
+
+export default ItemStatistics
